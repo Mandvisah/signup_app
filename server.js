9dcccb38e66f448da90681fa2db77739f81b4f1d
Mandvisah <mandvigupta35@gmail.com>
2025-07-09 00:01:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const User = require('./models/User');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/signup_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup', { error: null });
+});
+
+app.post('/signup', async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    // Check for existing username or email
+    const existingUser = await User.findOne({ $or: [ { username }, { email } ] });
+    if (existingUser) {
+      return res.status(400).render('signup', { error: 'Username or email already exists.' });
+    }
+    const user = new User({ username, email, password });
+    await user.save();
+    res.redirect('/signin');
+  } catch (err) {
+    res.status(500).render('signup', { error: 'Server error. Please try again.' });
+  }
+});
+
+app.get('/signin', (req, res) => {
+  res.render('signin');
+});
+
+app.post('/signin', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username, password });
+  if (user) {
+    res.send('Sign in successful!');
+  } else {
+    res.send('Invalid credentials');
+  }
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
