6fa2efa74e821297696a029dbc6a419a461051de
Mandvisah <mandvigupta35@gmail.com>
2025-07-18 19:05:00 +0530

    Improve server startup log message format for clarity

--- server.js
+++ server.js
@@ -55,4 +55,6 @@ app.post('/signin', async (req, res) => {
   } 
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
